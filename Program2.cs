9076db60d7dc87771cc50707d41f96a2bcbfe090
FaqWam <114764212+FaqWam@users.noreply.github.com>
2022-10-11 22:49:11 +0300

    15 задача

---
+++ Program2.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int dayNumber = int.Parse(Console.ReadLine());
+
+if (dayNumber > 5 && dayNumber < 8 )
+{
+      Console.Write("Да");     
+}
+else
+{
+      Console.Write("Нет");
+}
