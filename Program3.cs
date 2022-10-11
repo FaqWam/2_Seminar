29ee4bc94a3faff95afa8196a1f78860d067b2ec
FaqWam <114764212+FaqWam@users.noreply.github.com>
2022-10-11 21:57:08 +0300

    13 Задача

---
+++ Program3.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a / 100 > 0) 
+Console.WriteLine("Третья цифра " + a % 10);
+else 
+Console.WriteLine("У числа нет третьей цифры");
